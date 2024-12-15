382a9597074f0428bfa620e9330a97ab3d323058
Anton Trapeznikov <mir9635@yandex.ru>
2024-12-15 21:44:37 +0300

    commit

---
+++ src/main/java/org/springpattern/myexceptions/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package org.springpattern.myexceptions;
+
+public class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
