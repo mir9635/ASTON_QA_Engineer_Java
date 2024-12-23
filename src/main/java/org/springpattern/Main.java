eb0b3000b421a532da14cd62bfb0b56d7b173756
Anton Trapeznikov <mir9635@yandex.ru>
2024-12-23 17:59:10 +0300

    refactoring

--- src/main/java/org/springpattern/Main.java
+++
@@ -1,9 +0,0 @@
-package org.springpattern;
-
-import org.springpattern.Lesson_14.FactorialCalculator;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(FactorialCalculator.getFactorial(534));
-    }
-}
